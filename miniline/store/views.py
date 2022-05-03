90f0aa4d85c147e52ec801e1798dbd2c2329fcec
EDmitryBJ <edv.busines@gmail.com>
2022-05-03 18:34:41 +0500

    HW 3.2: added register, login, logout

--- miniline/store/views.py
+++ miniline/store/views.py
@@ -1,33 +1,40 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
-# Create your views here.
+@login_required
 def about(request):
-    context = {}
+    context = {'user': request.user.attr_name}
     return render(request, 'store/about.html', context)
 
 
+@login_required
 def contact(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'store/contact.html', context)
 
 
+@login_required
 def index(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'store/index.html', context)
 
 
+@login_required
 def products(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'store/products.html', context)
 
 
+@login_required
 def single_product(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'store/single-product.html', context)
 
 
+@login_required
 def cart(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'store/cart.html', context)
